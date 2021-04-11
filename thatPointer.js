fc8584c2a9475ef1d06a97c6a4b9366ee4e69fe7
WDRK <lllrrrlrssr@gmail.com>
2021-04-11 13:29:42 +0900

    this 포인터의 값을 that 포인터에 전달해서 메서드의 내부함수가 활용함

---
+++ thatPointer.js
@@ -0,0 +1,21 @@
+const value = 1;
+
+const obj = {
+  value: 100,
+  foo: function () {
+    const that = this;
+    console.log(`that === this is '${that === this}'`);
+    console.log(`foo's this : ${this}`);
+    console.log(`foo's this.value : ${this.value}`);
+
+    function bar() {
+      console.log(`bar's this : ${this}`);
+      console.log(`bar's this.value : ${this.value}`);
+      console.log(`bar's that : ${that}`);
+      console.log(`bar's that.value : ${that.value}`);
+    }
+    bar();
+  },
+};
+
+obj.foo();
