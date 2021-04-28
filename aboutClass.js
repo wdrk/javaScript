14247f839462100f147568a3c4649619f795f0ed
WDRK <lllrrrlrssr@gmail.com>
2021-04-28 22:03:23 +0900

    클래스의 타입, 클래스 내부 메서드의 위치확인, 메서드의 이름 가져오기

--- aboutClass.js
+++ aboutClass.js
@@ -9,5 +9,14 @@ class User {
   }
 }
 
-const user = new User('wdrk');
-user.sayHi();
+// 클래스는 function 이다
+console.log(typeof User); /* function */
+
+// 정확히는 생성자 메서드와 동일하다
+console.log(User === User.prototype.constructor); /* true */
+
+// 클래스는 내부에서 정의한 메서드를 User.prototype에 저장한다
+console.log(User.prototype.sayHi);
+
+// 프로포타입에 있는 메서드의 이름을 가져오는 방법
+console.log(Object.getOwnPropertyNames(User.prototype));
