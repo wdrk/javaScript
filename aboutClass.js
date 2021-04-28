0827b4d6d7061028f73535d89f6f8d7c6564a3c2
WDRK <lllrrrlrssr@gmail.com>
2021-04-28 22:13:33 +0900

    함수의 리턴값으로 클래스를 생성하게끔 하는 방법

--- aboutClass.js
+++ aboutClass.js
@@ -1,11 +1,13 @@
 'use strict';
 
-// 클래스 표현식으로 클래스를 정의하는 방법
-const User = class {
-  sayHi() {
-    console.log('hello');
-  }
+// 리턴값으로 클래스를 생성하는 방법
+const makeClass = (phrase) => {
+  return class {
+    sayHi() {
+      console.log(phrase);
+    }
+  };
 };
 
-// new 연산으로 생성한 객체의 sayHi() 함수를 호출하는 구조
-new User().sayHi();
+const User = makeClass('Hello');
+new User().sayHi(); /* Hello */
