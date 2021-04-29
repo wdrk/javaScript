4af71a2b03fd923b4e99476d597eeec5f567586e
WDRK <lllrrrlrssr@gmail.com>
2021-04-29 16:29:46 +0900

    Array.of() 로 배열을 생성하면, 어느 자료형의 값이던지 한 개만 넘겼을때 배열의 0번 요소로 쓴다

--- aboutArray.js
+++ aboutArray.js
@@ -1,24 +1,14 @@
-// Array 생성자는 전달한 인자의 타입과 개수에 따라 동작이 다르다
+'use strict';
 
-// 숫자 값 하나만을 전달하는 경우,
-// 배열의 0번 인덱스 값으로 들어가는게 아니라 length값으로 쓴다
-let items = new Array(2);
-console.log(items.length); /* 2 */
-console.log(items[0]); /* undefined */
-console.log(items[1]); /* undefined */
+let items = Array.of(1, 2);
+console.log(items.length);
+console.log(items[0]);
+console.log(items[1]);
 
-// 숫자가 아닌 값 하나만을 전달하는 경우,
-// 배열의 유일한 값으로 쓴다
-items = new Array('2');
-console.log(items.length); /* 1 */
-console.log(items[0]); /* "2" */
+items = Array.of(2);
+console.log(items.length);
+console.log(items[0]);
 
-items = new Array(1, 2);
-console.log(items.length); /* 2 */
-console.log(items[0]); /* 1 */
-console.log(items[1]); /* 2 */
-
-items = new Array(3, '2');
-console.log(items.length); /* 2 */
-console.log(items[0]); /* 3 */
-console.log(items[1]); /* "2" */
+items = Array.of('2');
+console.log(items.length);
+console.log(items[0]);
