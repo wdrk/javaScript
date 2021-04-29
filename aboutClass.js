3da82797785f745e7ee78e323f4dd9892b6f9dc6
WDRK <lllrrrlrssr@gmail.com>
2021-04-29 14:44:46 +0900

    부모 클래스의 메서드를 재정의(오버라이드) 하는 구조

--- aboutClass.js
+++ aboutClass.js
@@ -15,6 +15,12 @@ class Square extends Rectangle {
   constructor(length) {
     super(length, length);
   }
+
+  // 함수 재정의 (오버라이드)
+  getArea() {
+    console.log('Square.getArea()');
+    return this._length * this._length;
+  }
 }
 
 const square = new Square(3);
