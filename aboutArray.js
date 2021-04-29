8769bf025e031d020b77b3f8d508afce93180bc2
WDRK <lllrrrlrssr@gmail.com>
2021-04-29 16:26:56 +0900

    Array()로 배열을 생성할때 숫자값 하나만을 전달하면 그 값을 length 값으로 쓰는 문제가 있다

--- aboutArray.js
+++ aboutArray.js
@@ -1,9 +1,14 @@
 // Array 생성자는 전달한 인자의 타입과 개수에 따라 동작이 다르다
+
+// 숫자 값 하나만을 전달하는 경우,
+// 배열의 0번 인덱스 값으로 들어가는게 아니라 length값으로 쓴다
 let items = new Array(2);
 console.log(items.length); /* 2 */
 console.log(items[0]); /* undefined */
 console.log(items[1]); /* undefined */
 
+// 숫자가 아닌 값 하나만을 전달하는 경우,
+// 배열의 유일한 값으로 쓴다
 items = new Array('2');
 console.log(items.length); /* 1 */
 console.log(items[0]); /* "2" */
