92d5ed1f19ea1f72f8634ce33ea13562275322bc
WDRK <lllrrrlrssr@gmail.com>
2021-04-29 13:36:55 +0900

    클래스를 함수의 인자, 함수의 리턴값, 익명 클래스로 활용하는 방법

--- aboutClass.js
+++ aboutClass.js
@@ -1,25 +1,15 @@
 'use strict';
 
-class User {
-  constructor(name) {
-    this.name = name;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  set name(phrase) {
-    if (phrase.length < 4) {
-      console.log('Too short');
-      return;
-    } else {
-      this._name = phrase;
-    }
-  }
+function createObject(classDef) {
+  return new classDef();
 }
 
-const user = new User('wdrk');
-console.log(user.name); /* wdrk */
+const obj = new createObject(
+  class {
+    sayHi() {
+      console.log('Hi');
+    }
+  }
+);
 
-const user2 = new User(''); /* Too short */
+obj.sayHi();
