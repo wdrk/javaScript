b3fc5dffa3d26e1e6785b53362efde5f3bd7c4db
WDRK <lllrrrlrssr@gmail.com>
2021-04-30 16:11:18 +0900

    타입 배열은 일반 배열이 갖고 있는 메서드를 갖고 있고, 타입 배열을 반환한다

--- aboutArray.js
+++ aboutArray.js
@@ -1,14 +1,9 @@
 'use strict';
 
-// 타입 배열 선언과 정의를 동시에 하기
-let ints = new Int16Array([25, 50]);
+const ints = new Int16Array([25, 50]);
+const mapped = ints.map((v) => v * 2);
 
-console.log(ints.length); /* 2 */
-console.log(ints[0]); /* 25 */
-console.log(ints[1]); /* 50 */
-
-ints[0] = 1;
-ints[1] = 2;
-
-console.log(ints[0]); /* 1 */
-console.log(ints[1]); /* 2 */
+console.log(mapped.length);
+console.log(mapped[0]);
+console.log(mapped[1]);
+console.log(mapped instanceof Int16Array);
