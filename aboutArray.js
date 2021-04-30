85b9d4104f7058d4a120c0f7916d00973b437cc6
WDRK <lllrrrlrssr@gmail.com>
2021-04-30 14:34:44 +0900

    배열의 find(), findIndex() 메서드 사용 예제

--- aboutArray.js
+++ aboutArray.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const translate = (...args) => {
-  return Array.from(args, (value) => value + 1);
-};
+const numbers = [25, 30, 35, 40, 45];
 
-let numbers = translate(1, 2, 3);
-console.log(numbers);
+// 콜백 함수가 처음 true를 반환한 값을 반환한다
+console.log(numbers.find((n) => n > 33)); /* 35 */
+
+// 콜백 함수가 처음 true를 반환한 값의 인덱스를 반환한다
+console.log(numbers.findIndex((n) => n > 33)); /* 2 */
