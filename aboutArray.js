17dacdb4449c4b03c1740ac39304c3cb6f37c4d4
WDRK <lllrrrlrssr@gmail.com>
2021-04-30 15:22:43 +0900

    Int8Array 와 같이 타입을 명시한 view 인스턴스 생성하기

--- aboutArray.js
+++ aboutArray.js
@@ -1,14 +1,12 @@
 'use strict';
 
-// 배열 버퍼와 뷰 선언
-const buffer = new ArrayBuffer(2);
-const view = new DataView(buffer);
+const buffer = new ArrayBuffer(10);
+const view1 = new Int8Array(buffer);
+const view2 = new Int8Array(buffer, 5, 2);
 
-// DataView의 setter 함수로 (오프셋, 값) 지정
-view.setInt8(0, 5);
-view.setUint8(1, -1);
-
-// 원하는 비트 단위와 오프셋을 조합해서 값을 읽어올 수 있다
-console.log(view.getInt8(0)); /* 5 */
-console.log(view.getInt8(1)); /* -1 */
-console.log(view.getInt16(0)); /* 1535 */
+console.log(view1.buffer === buffer); /* true */
+console.log(view2.buffer === buffer); /* true */
+console.log(view1.byteOffset); /* 0 */
+console.log(view2.byteOffset); /* 5 */
+console.log(view1.byteLength); /* 10 */
+console.log(view2.byteLength); /* 2 */
