6c4d0f80ed371bf75a3ead532511a76aefe06dde
WDRK <lllrrrlrssr@gmail.com>
2021-04-30 16:02:13 +0900

    타입 배열 선언과 정의를 동시에 하기. 요소에 직접 접근해서 값 변경 가능

--- aboutArray.js
+++ aboutArray.js
@@ -1,11 +1,14 @@
 'use strict';
 
-// 배열 버퍼를 먼저 만들지 않아도 자동으로 새로운 버퍼를 만든다
-// 생성할때 전달하는 숫자는 타입별로 배열 길이(개수)다
-const ints = new Int16Array(2);
-const floats = new Float32Array(5);
+// 타입 배열 선언과 정의를 동시에 하기
+let ints = new Int16Array([25, 50]);
 
-console.log(ints.byteLength); /* 4 */
 console.log(ints.length); /* 2 */
-console.log(floats.byteLength); /* 20 */
-console.log(floats.length); /* 5 */
+console.log(ints[0]); /* 25 */
+console.log(ints[1]); /* 50 */
+
+ints[0] = 1;
+ints[1] = 2;
+
+console.log(ints[0]); /* 1 */
+console.log(ints[1]); /* 2 */
