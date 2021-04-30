999f3d4bbcd204ecdfc7daba8fce8e3adb014738
WDRK <lllrrrlrssr@gmail.com>
2021-04-30 15:27:01 +0900

    배열 버퍼를 미리 만들지 않아도 타입을 명시한 view를 만들 수 있다

--- aboutArray.js
+++ aboutArray.js
@@ -1,12 +1,11 @@
 'use strict';
 
-const buffer = new ArrayBuffer(10);
-const view1 = new Int8Array(buffer);
-const view2 = new Int8Array(buffer, 5, 2);
+// 배열 버퍼를 먼저 만들지 않아도 자동으로 새로운 버퍼를 만든다
+// 생성할때 전달하는 숫자는 타입별로 배열 길이(개수)다
+const ints = new Int16Array(2);
+const floats = new Float32Array(5);
 
-console.log(view1.buffer === buffer); /* true */
-console.log(view2.buffer === buffer); /* true */
-console.log(view1.byteOffset); /* 0 */
-console.log(view2.byteOffset); /* 5 */
-console.log(view1.byteLength); /* 10 */
-console.log(view2.byteLength); /* 2 */
+console.log(ints.byteLength); /* 4 */
+console.log(ints.length); /* 2 */
+console.log(floats.byteLength); /* 20 */
+console.log(floats.length); /* 5 */
