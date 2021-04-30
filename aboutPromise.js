ef54783c1233a7c6e86b952dbbfeea13db8ab1e6
WDRK <lllrrrlrssr@gmail.com>
2021-04-30 19:48:57 +0900

    Promise.all로 다수의 Promise를 배열로 관리하기

--- aboutPromise.js
+++ aboutPromise.js
@@ -1,24 +1,20 @@
 'use strict';
 
-// 성공 핸들러가 에러를 발생시키고 반환한 프로미스의 catch에 에러를 전달한다
 const p1 = new Promise((resolve, reject) => {
   resolve(42);
 });
-p1.then((value) => {
-  throw new Error('Boom!');
-}).catch((error) => {
-  console.log(`p1 error : ${error.message}`);
-});
-
-// p2 실행자가 에러를 발생시키고 실패 핸들러를 실행한다
-// 실패 핸들러가 다시 에러를 발생시키고 두번째 프로미스의 실패 핸들러가 처리한다
 const p2 = new Promise((resolve, reject) => {
-  throw new Error('Explosion!');
+  resolve(43);
+});
+const p3 = new Promise((resolve, reject) => {
+  resolve(44);
 });
 
-p2.catch((error) => {
-  console.log(`p2 error : ${error.message}`);
-  throw new Error('Boom!');
-}).catch((error) => {
-  console.log(`p2 error : ${error.message}`);
+const p4 = Promise.all([p1, p2, p3]);
+
+p4.then((value) => {
+  console.log(Array.isArray(value)); /* true */
+  console.log(value[0]); /* 42 */
+  console.log(value[1]); /* 43 */
+  console.log(value[2]); /* 44 */
 });
