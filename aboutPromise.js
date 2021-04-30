288264ca2a035c8d53ebac4b2de372c676665f3e
WDRK <lllrrrlrssr@gmail.com>
2021-04-30 19:21:08 +0900

    Promise에 내장되어 있는 try-throw-catch를 간단히 사용하는 방법

--- aboutPromise.js
+++ aboutPromise.js
@@ -1,18 +1,18 @@
 'use strict';
 
-// Promise는 선언하면 즉시 실행된다
-const promise = new Promise((resolve, reject) => {
-  console.log('Promise');
-  resolve();
-});
-console.log('Hi!');
+// const promise = new Promise((resolve, reject) => {
+//   try {
+//     throw new Error('Explosion!');
+//   } catch (ex) {
+//     reject(ex);
+//   }
+// });
 
-// promise의 resolve, reject는 비동기 실행이어서
-// 자바스크립트 엔진이 작업큐의 맨 마지막에 등록하므로
-// console.log('Hi2') 보다 늦게 실행된다
-const promise2 = new Promise((resolve, reject) => {
-  console.log('Promise2');
-  resolve();
+// 위 예제를 간단히 하는 방법
+const promise = new Promise((resolve, reject) => {
+  throw new Error('Explosion!');
 });
-promise2.then(() => console.log('resolved'));
-console.log('Hi2');
+
+promise.catch((error) => {
+  console.log(error.message); /* Explosion! */
+});
