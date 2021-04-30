a4560ea07d9b583d8213ac9da107eb3d8b11f2d3
WDRK <lllrrrlrssr@gmail.com>
2021-04-30 22:26:54 +0900

    promise chaining으로 값을 이어받고 새로운 promise를 전달하는 방식

--- dreamCodingPromise.js
+++ dreamCodingPromise.js
@@ -23,3 +23,22 @@ promise
       'finally'
     ); /* promise의 성공/실패 여부와 상관없이 마지막에 무조건 호출하는 함수 */
   });
+
+// 3. Promise Chaining
+const fetchNumber = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(1);
+  }, 1000);
+});
+
+fetchNumber
+  .then((num) => num * 2)
+  .then((num) => num * 3)
+  .then((num) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(num - 1);
+      }, 1000);
+    });
+  })
+  .then((num) => console.log(num));
