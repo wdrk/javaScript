546093227742ff37af2fab23ccfb06869bc20742
WDRK <lllrrrlrssr@gmail.com>
2021-05-01 16:29:20 +0900

    Promise.race() 로 가장 먼저 반환한 프로미스만 반환하는 방법

--- dreamCodingAsync.js
+++ dreamCodingAsync.js
@@ -33,9 +33,17 @@ async function pickFruits() {
 pickFruits().then(console.log);
 
 // 3. useful Promise APIs
+
+// 여러 프로미스를 동시에 실행해서 전부 받을때까지 기다리는 API
 function pickAllFruits() {
   return Promise.all([getApple(), getBanana()]).then((fruits) =>
     fruits.join(' + ')
   );
 }
 pickAllFruits().then(console.log);
+
+// 여러 프로미스를 동시에 실행해서 가장 먼저 반환한 프로미스만 반환하는 API
+function pickOnlyOne() {
+  return Promise.race([getApple(), getBanana()]);
+}
+pickOnlyOne().then(console.log);
