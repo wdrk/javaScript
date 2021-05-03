ee021ffc17f84edde0a118d5f251feff9b33c911
WDRK <lllrrrlrssr@gmail.com>
2021-05-03 22:02:27 +0900

    export할때 대상의 이름을 바꿔서 export 하는 방법

--- aboutModule.js
+++ aboutModule.js
@@ -1,35 +1,8 @@
 'use strict';
 
-// 데이터 익스포트
-export var color = 'red';
-export let name = 'Nicholas';
-export const magicNumber = 7;
-
-// 함수 익스포트
-export function sum(num1, num2) {
+function sum(num1, num2) {
   return num1 + num2;
 }
 
-export function setName(newName) {
-  name = newName;
-}
-
-// 클래스 익스포트
-export class Rectangle {
-  constructor(length, width) {
-    this._length = length;
-    this._width = width;
-  }
-}
-
-// 이 함수는 모듈에 비공개
-function subtract(num1, num2) {
-  return num1 - num2;
-}
-
-// 함수 정의
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-
-export { multiply };
+// export 할때 변수, 함수, 클래스의 이름을 바꾸는 방법
+export { sum as add };
--- importModule.js
+++ importModule.js
@@ -1,8 +1,4 @@
-import { name, setName } from './aboutModule.js';
+// import는 export 할때 바꾼 이름으로 해야 함
+import { add } from './aboutModule.js';
 
-console.log(name); /* Nicholas */
-setName('Greg');
-console.log(name); /* Greg */
-
-// import로 가져온 변수는 해당 모듈 쪽 함수에서만 값을 바꿀 수 있다
-// name = 'Nicholas'; /* Error */
+console.log(add(1, 2));
