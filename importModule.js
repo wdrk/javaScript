d35b3a7fb5792366de08d22c11418064a8d4e919
WDRK <lllrrrlrssr@gmail.com>
2021-05-04 14:39:02 +0900

    import와 export를 분리해서 수행하는 방법

--- importModule.js
+++ importModule.js
@@ -1,9 +1,9 @@
 // 바인딩을 다시 export 하기
-export { sum } from './aboutModule.js';
+// export { sum } from './aboutModule.js';
 
 // 다른 방법
-// import { sum } from './aboutModule.js';
-// export { sum }
+import { sum } from './aboutModule.js';
+export { sum };
 
 // 바인딩을 import하고 다른 이름으로 export 하는 방법
 // export { sum as add } from './aboutModule.js';
