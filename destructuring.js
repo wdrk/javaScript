c01bea9736a8f0fc206b629af2c193aff6bf5ab2
WDRK <lllrrrlrssr@gmail.com>
2021-05-07 16:51:19 +0900

    객체 안에 존재하지 않는 프로퍼티에 초기값을 설정

--- destructuring.js
+++ destructuring.js
@@ -1,11 +1,14 @@
 let node = {
-    type: 'Identifier',
-    name: 'foo',
-  },
-  type = 'Literal',
-  name = 5;
+  type: 'Identifier',
+  name: 'foo',
+};
 
-({ type, name } = node);
+let { type, name, value } = node;
 
-console.log(type);
-console.log(name);
+console.log(type); /* Identifier */
+console.log(name); /* foo */
+console.log(value); /* undefined */
+
+// 객체 안에 존재하지 않는 프로퍼티에 초기값을 설정
+let { basicValue = true } = node;
+console.log(basicValue); /* true */
