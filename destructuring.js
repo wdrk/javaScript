99b1f5d8171ad2705a4a84301b72481c96c72efc
WDRK <lllrrrlrssr@gmail.com>
2021-05-08 22:01:47 +0900

    메서드를 이름이 다른 지역 변수에 할당하기

--- destructuring.js
+++ destructuring.js
@@ -1,14 +1,11 @@
+'use strict';
+
 let node = {
   type: 'Identifier',
   name: 'foo',
 };
 
-let { type, name, value } = node;
+let { type: localType, name: localName } = node;
 
-console.log(type); /* Identifier */
-console.log(name); /* foo */
-console.log(value); /* undefined */
-
-// 객체 안에 존재하지 않는 프로퍼티에 초기값을 설정
-let { basicValue = true } = node;
-console.log(basicValue); /* true */
+console.log(localType); /* Identifier */
+console.log(localName); /* foo */
