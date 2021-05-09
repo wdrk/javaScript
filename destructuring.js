3c33a1df950e3ffa475ad7a93d5966a6510f805e
WDRK <lllrrrlrssr@gmail.com>
2021-05-09 21:38:31 +0900

    구조분해 문법을 이용해서 손쉽게 변수의 값을 교차하는 방법

--- destructuring.js
+++ destructuring.js
@@ -1,8 +1,20 @@
 'use strict';
 
-const colors = ['red', 'green', 'blue'];
+/* ES6 이전에 값을 교환하던 방법 */
+let a = 1;
+let b = 2;
+let tmp;
 
-const [firstColor, secondColor] = colors;
+tmp = a;
+a = b;
+b = tmp;
 
-console.log(firstColor); /* red */
-console.log(secondColor); /* green */
+/* 구조분해 문법을 이용해서 손쉽게 변수의 값을 교환하는 방법 */
+
+let c = 1;
+let d = 2;
+
+[c, d] = [d, c];
+
+console.log(c); /* 2 */
+console.log(d); /* 1 */
